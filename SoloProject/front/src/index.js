a642d2ed0412dc9283b457ad5e000567be36c4e1
Yim <yimjh2309@gmail.com>
2022-03-22 22:41:43 +0900

    백엔드 서버 시작`

--- SoloProject/front/src/index.js
+++ SoloProject/front/src/index.js
@@ -4,13 +4,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollTop from './ScrollTop';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
   <React.StrictMode>
     <ScrollTop />
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
